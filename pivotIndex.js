6a89d23bdbae488fdc8fed096430e3b7cbf22605
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2019-12-09 15:28:00 -0500

    pivotIndex

---
+++ pivotIndex.js
@@ -0,0 +1,19 @@
+function pivotIndex(nums){
+    // find sum of all numbers 
+    let sum = 0 
+
+    for(let i = 0; i < nums.length; i++){
+        sum += nums[i]
+    }
+    let leftSum = 0 
+    for(let i = 0; i <nums.length; i++){
+        if(leftSum == sum - leftSum - nums[i]){
+            return i
+        }
+        leftSum += nums[i]
+    }
+    return -1
+
+}
+
+console.log(pivotIndex([1, 7, 3, 6, 5, 6]))
