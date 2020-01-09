50061833962d7b5af332884b26db0f37498a492f
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-01-08 21:39:28 -0500

    diff arrays

--- freeCodeCamp/sumNumbersInRange.js
+++ freeCodeCamp/sumNumbersInRange.js
@@ -18,6 +18,18 @@ function sumAll(arr) {
     return range.reduce((acc, current) => acc + current)
     
   }
+
+  function sumAll(arr) {
+    var max = Math.max(arr[0], arr[1]);
+    var min = Math.min(arr[0], arr[1]);
+    var temp = 0;
+    for (var i = min; i <= max; i++) {
+      temp += i;
+    }
+    return temp;
+  }
+  
+  sumAll([1, 4])
   
   sumAll([1, 4]);
   
