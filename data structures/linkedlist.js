4cfbcb00c4d93e5fc9cfe26f1e67cc08b866c298
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-01-11 20:24:50 -0500

    linked list ds

---
+++ data structures/linkedlist.js
@@ -0,0 +1,20 @@
+10-->15-->16
+
+let myLinkedList = {
+    head: {
+        value:  10, 
+        next: {
+            value: 5,
+            next:{
+                value:16,
+                next:null
+            }
+        }
+    }
+}
+
+class LinkedList {
+    constructor(value)
+}
+
+const myLinkedList = new LinkedList()
