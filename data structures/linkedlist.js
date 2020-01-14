789724a1c42f250f30eb5eb35dc83dd3d406b9bc
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-01-13 20:18:14 -0500

    append and prepend

--- data structures/linkedlist.js
+++ data structures/linkedlist.js
@@ -13,6 +13,13 @@ let ll = {
     }
 }
 
+class Node {
+    constructor(value){
+        this.value = value
+        this.next = null
+    }
+}
+
 class LinkedList {
     constructor(value){
         this.head = {
@@ -24,10 +31,52 @@ class LinkedList {
     }
 
     append(value){
-        
+        // create new node
+       const newNode = new Node(value)
+       // point old tail to new tail
+        this.tail.next = newNode
+        //make new node tail node
+        this.tail = newNode
+        //increase length
+        this.length += 1
+        //returns linked list
+        return this
     }
+
+    prepend(value){
+        const newNode = new Node(value)
+
+        newNode.next = this.head
+
+        this.head = newNode
+
+        this.length += 1 
+
+        return this.linkedList
+
+    }
+
+    printList(){
+        const array = [];
+        let currentNode = this.head;
+        while (currentNode != null) {
+            array.push(currentNode.value)
+            currentNode = currentNode.next
+        }
+        return array 
+    }
+
+    insert(index, value){
+
+    }
+
+
 }
 
 let myLinkedList = new LinkedList(10)
 
-console.log(myLinkedList)
+myLinkedList.append(5)
+myLinkedList.append(16)
+myLinkedList.prepend(12)
+console.log(myLinkedList.printList())
+// console.log(myLinkedList)
