0f67df32e294d35fd92e2960a5f2864ef94a51fd
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-01-16 23:43:33 -0500

    binary search tree

---
+++ data structures/bst.js
@@ -0,0 +1,29 @@
+class Node {
+    constructor(value){
+        this.left = null;
+        this.right = null;  
+        this.value = value
+    }
+}
+
+class BinarySearchTree {
+    constructor(){
+        this.root = null;
+    }
+
+    insert(value){
+
+    }
+    lookup(value){
+
+    }
+}
+
+const tree = new BinarySearchTree()
+
+function traverse(node){
+    const tree = {value: node.value}
+    tree.left = node.left === null ? null : traverse(node.left);
+    tree.right = node.right === null ? null : traverse(node.right)
+    return tree;
+}
