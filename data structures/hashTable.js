c15db888436bb0018e5b55cb6db68cc66d06da4e
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-01-20 20:37:31 -0500

    hash function keys method

--- data structures/hashTable.js
+++ data structures/hashTable.js
@@ -36,6 +36,16 @@ class HashTable {
             }
             return undefined 
         }
+
+        keys(){
+            const keysArray = []
+            for(let i = 0; i < this.data.length; i++){
+                if(this.data[i]){
+                    keysArray.push(this.data[i][0][0])
+                }
+            }
+            return keysArray;
+        }
     }
 
 
