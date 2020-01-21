98a729a341d6c10534d062b0693e6a76a6466b7a
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-01-20 20:33:57 -0500

    get and set

--- data structures/hashTable.js
+++ data structures/hashTable.js
@@ -10,7 +10,34 @@ class HashTable {
         }
         return hash 
     }
-}
+
+    set(key, value){
+        // set address to number returned by hash function
+        let address = this._hash(key)
+        //check if data does not exist
+        if(!this.data[address]){
+        //if it does not exist add that data
+            this.data[address] = [];
+            this.data[address].push([key, value])
+        } 
+          this.data[address].push([key, value])
+          return this.data
+        }
+
+        get(key){
+            let address = this._hash(key)
+            const currentBucket = this.data[address]
+            if(currentBucket){
+                for(let i; i < currentBucket.length; i++){
+                    if(currentBucket[i][0] === key){
+                        return currentBucket[i][1]
+                    }
+                }
+            }
+            return undefined 
+        }
+    }
+
 
 const myHashTable = new HashTable(50)
 myHashTable.set('banana', 10000)
