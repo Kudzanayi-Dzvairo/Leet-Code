3400b2746aa232be9c969da95cb2173e37cf3d2a
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-01-28 00:24:00 -0500

    bfs or dfs

---
+++ algorithms/bfsORdfs.js
@@ -0,0 +1,17 @@
+//If you know the solution is not far from the root of the tree
+bfs
+
+//If the tree is very deep and the solutiosn are rare
+bfs (dfs will take too long)
+
+//if the tree is very wide 
+dfs (bfs will need too much memory)
+
+//if the solutions are frequent but located deep in the tree
+dfs
+
+//determining whether a pathe exists between two nodes
+dfs
+
+//finding the shortest path 
+bfs
