e50a555735f6e5b4c59f4b8b053e9cfcf532cb9d
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-02-05 23:06:34 -0500

    map debris

--- leetcode/highFive.js
+++ leetcode/highFive.js
@@ -1,22 +1,17 @@
-var highFive = function(item){
+var highFive = function(items){
 
-    grades = []
-
-    //iterate over array and identify student id 
-    studentId = 1 
-   
-    for(let i = 0; i < item.length; i++){
-        // console.log(item[i])
-        for(let j = 0; j < item[i].length; j++){
-            if(item[i][0] = studentId) {
-                results = []
-                results.push(item[i][1])
-                results.sort((function(a, b){return b-a}))
-                console.log(results)
-
-            }
+    const dictionary = items.reduce((acc, cur) => {
+        const key = cur[0];
+        const val = cur[1];
+    
+        if (acc[key]) {
+            acc[key].push(val);
+        } else {
+            acc[key] = [val]
         }
-    }
+    
+        console.log(acc);
+    }, {})
 }
 
 console.log(highFive([[1,91],[1,92],[2,93],[2,97],[1,60],[2,77],[1,65],[1,87],[1,100],[2,100],[2,76]]))
