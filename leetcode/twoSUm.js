cac74e60be786635c9c566252d1229ebd97cd633
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-02-15 23:24:44 -0500

    two sum alternate

--- leetcode/twoSUm.js
+++ leetcode/twoSUm.js
@@ -10,3 +10,14 @@ var twoSum = function(nums, target) {
 	}
 	return [];
 }
+
+const twoSum = (nums, target) => {
+    const m = new Map();
+    for (const [i, v] of nums.entries()) {
+      const k = target - v;
+      if (m.has(k)) {
+        return [m.get(k), i];
+      }
+      m.set(v, i);
+    }
+  };
