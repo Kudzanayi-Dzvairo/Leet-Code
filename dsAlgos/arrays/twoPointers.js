b8273b7455cbaeabce961942b4d3fb116ed735cc
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-03-05 23:52:24 -0500

    two pointers

---
+++ dsAlgos/arrays/twoPointers.js
@@ -0,0 +1,14 @@
+//Given a sorted array A( sorted in ascending order),having N intergers, 
+//find if there exists any pair of elements (A[i], A[j]) such that 
+//their sum is equal to X
+//
+//input: A = [2,3,4,5,6,7,8,9], k = 10 
+//Output: true
+
+const arr = [2,3,4,5,6,7,8,9]
+const k = 11;
+
+function findPairs(arr, k){
+
+}
+console.log(findPairs(arr, k))
