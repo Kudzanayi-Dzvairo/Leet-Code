3207e4961a25b311d5e3bb77119758913ed02af6
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-03-10 09:58:19 -0400

    missing number

---
+++ dsAlgos/arrays/missingNumber.js
@@ -0,0 +1,12 @@
+var missingNumber = function(nums) {
+    
+    nums.sort((a,b) => a - b);
+    
+    if(nums[0] === 1) return 0
+    
+    for(let i = 0; i < nums.length; i++){
+        if((nums[i] + 1) !== (nums[i+1]) ){
+            return nums[i] + 1
+        }
+    }
+};
