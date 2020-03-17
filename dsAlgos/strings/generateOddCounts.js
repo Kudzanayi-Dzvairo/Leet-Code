6390916a64c171bbfdaf3561c213b7232ddc0df1
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-03-17 12:24:32 -0400

    imporoved odd number string solution

--- dsAlgos/strings/generateOddCounts.js
+++ dsAlgos/strings/generateOddCounts.js
@@ -1,26 +1,19 @@
 var generateTheString = function(n) {
     
-    //if n is an even number we do no want to alternate 
-     // we want to add a number once and the another the rest of the times
      
-     //if n is odd we want to alternate 
-     
-     //solve for even first 
-     
-     let result = ''
-     let alternate = 'y' 
-     
-     for(let i = 0; i < n; i++){
-       if(n % 2 === 1){
-           result += 'x'
-       } 
-         
-       if(n % 2 === 0){
-           result += 'x'
-           result += alternate.repeat(n-1)
-           return result 
-       }
-     }
+    let result = ''
+    
+    if(n % 2 === 1){
+        result += 'x'.repeat(n)
+        return result
+    }
+    
+    if(n % 2 === 0){
+        result += 'x'
+        result += 'y'.repeat(n-1)
+        return result
+    }
+      
      
      return result
      
