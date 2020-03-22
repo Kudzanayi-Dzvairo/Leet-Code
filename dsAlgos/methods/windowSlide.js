549923e5f9d4f140e662e8d956c55c8c81b6043e
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-03-21 23:13:31 -0400

    solutions

--- dsAlgos/methods/windowSlide.js
+++ dsAlgos/methods/windowSlide.js
@@ -36,4 +36,51 @@ function maxSum(arr, k){
 }
 
 
+function maxSum2(arr, k) { 
+    //initialize result
+        let maxSum = 0 
+    for(let i = 0; i < arr.length - k; i++){
+             currentSum = 0 
+             for(let j = k; j < k; j++) {
+                 currentSum  += arr[i] - arr[i-k]
+                 
+                 max_sum = Math.max(currentSum, maxSum)
+              }
+         return maxSum
+    }
 
+}
+
+console.log(maxSum2(arr, k))
+
+import sys 
+INT_MIN = -sys.maxsize -1
+  
+def maxSum(arr, n, k): 
+  
+    # n must be greater than k 
+    if not n > k: 
+        print("Invalid") 
+        return -1
+  
+    # Compute sum of first window of size k 
+    max_sum = INT_MIN 
+    window_sum = sum([arr[i] for i in range(k)]) 
+  
+    # Compute sums of remaining windows by 
+    # removing first element of previous 
+    # window and adding last element of 
+    # current window. 
+    for i in range(n-k): 
+        window_sum = window_sum - arr[i] + arr[i + k] 
+        max_sum = max(window_sum, max_sum) 
+  
+    return max_sum 
+  
+# Driver code 
+arr = [1, 4, 2, 10, 2, 3, 1, 0, 20] 
+k = 4
+n = len(arr) 
+print(maxSum(arr, n, k)) 
+  
+# This code is contributed by Kyle McClay 
