819fbb76d2eb0bfeea0f7529a518833b735b5de7
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-03-28 23:21:28 -0400

    average time

--- interviewCake/arrays/mergeMeetingTimes.js
+++ interviewCake/arrays/mergeMeetingTimes.js
@@ -1,4 +1,21 @@
-var mergeMeetingTimes = (rangeArray){
+let times =   [
+    { startTime: 0,  endTime: 1 },
+    { startTime: 3,  endTime: 5 },
+    { startTime: 4,  endTime: 8 },
+    { startTime: 10, endTime: 12 },
+    { startTime: 9,  endTime: 10 },
+  ]
 
-    
+var mergeMeetingTimes = (rangeArray) => {
+       
+    rangeArray.sort((a,b) => a.startTime - b.startTime)
+      // goal is to find a time whenver every one is available
+      // integers represent the number of 30 min blocks from  9am 
+      // wirte a function that mergers ranges 
+
+      // takes array of multiple meetings time ranges and returns an array of condensed ranges
+
+     console.log(rangeArray)
 }
+
+console.log(mergeMeetingTimes(times))
