fe0b56338edaa667f5b76e625ca2207e18fdf138
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-04-04 23:38:43 -0400

    in fligth times

--- interviewCake/hashTable/inFlight.js
+++ interviewCake/hashTable/inFlight.js
@@ -1,4 +1,21 @@
-var InFlightTimes = (array, time){
-
+var inFlightTimes = (movieLengths, flightLength){
+    //create a set 
+    const movieLengthsSeen = new Set();
     
+    //check for movies we've seen so far
+    for(let i = 0; i < movieLengths ; i++){
+
+        const firstMovieLength = movieLength[i]
+        
+        //subtract current movie length from flight to find matching movie length time 
+        const matchingSecondMovieLength = flightLength - firstMovieLength
+        //see if matching movie length exists is set 
+        if(movieLengthsSeen.has(matchingSecondMovieLength)){
+            return true
+        }
+        
+        //if not add current movie to list of movies to choose from 
+        movieLengthsSeen.add(firstMovieLength)
+    }
+
 }
