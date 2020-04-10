e882dbe528903bfdb0f167154d377b421bdc8ced
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-04-10 17:00:10 -0400

    apple stock

--- interviewCake/greedy.js/appleStock.js
+++ interviewCake/greedy.js/appleStock.js
@@ -1,3 +1,29 @@
-function getMaxProfits(stocks){
-    
-}
+function getMaxProfit(stockPrices) {
+    let maxProfit = 0;
+  
+    // Go through every time
+    for (let outerTime = 0; outerTime < stockPrices.length; outerTime++) {
+  
+      // For each time, go through every other time
+      for (let innerTime = 0; innerTime < stockPrices.length; innerTime++) {
+  
+        // For each pair, find the earlier and later times
+        const earlierTime = Math.min(outerTime, innerTime);
+        const laterTime = Math.max(outerTime, innerTime);
+  
+        // And use those to find the earlier and later prices
+        const earlierPrice = stockPrices[earlierTime];
+        const laterPrice = stockPrices[laterTime];
+  
+        // See what our profit would be if we bought at the
+        // min price and sold at the current price
+        const potentialProfit = laterPrice - earlierPrice;
+  
+        // Update maxProfit if we can do better
+        maxProfit = Math.max(maxProfit, potentialProfit);
+      }
+    }
+  
+    return maxProfit;
+  }
+  
